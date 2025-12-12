d5e3add51747b8958a6d8bd794d5ac59ad50e78a
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-12 16:21:40 +0530

    Create cpp-solution.py

---
+++ 07_weather_api_fetcher/cpp-solution.py
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// For C++, live API fetching requires extra libraries (like cURL).
+// We'll use a static example for demonstration.
+
+int main(){
+    string city; cout << "Enter city: "; cin >> city;
+    cout << "Temperature: 15°C\nHumidity: 70%\nWind: 10 km/h\nFeels like: 14°C\n";
+    return 0;
+}
