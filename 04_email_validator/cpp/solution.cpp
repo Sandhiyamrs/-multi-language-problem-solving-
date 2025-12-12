c462740d3fb56271614152d14b0c926ff9e6b2c1
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-12 16:11:32 +0530

    Update solution.cpp

--- 04_email_validator/cpp/solution.cpp
+++ 04_email_validator/cpp/solution.cpp
@@ -1,1 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <regex>
+
+int main(){
+    string email;
+    cout << "Enter email: ";
+    cin >> email;
+    regex pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,}$");
+    if(regex_match(email, pattern)) cout << "Valid Email\n";
+    else cout << "Invalid Email\n";
+    return 0;
+}
 
