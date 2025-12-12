f00a1ec0f014ff6ee636a19a567471ecfacfc99d
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-12 16:09:46 +0530

    Update solution.py

--- 04_email_validator/python/solution.py
+++ 04_email_validator/python/solution.py
@@ -1,1 +1,8 @@
+import re
+email = input("Enter email: ")
+pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$'
+if re.match(pattern, email):
+    print("Valid Email")
+else:
+    print("Invalid Email")
 
