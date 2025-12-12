45bcb5f07b4921df4c6c7d5351473afe7638fa41
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-12 16:11:03 +0530

    Update Solution.java

--- 04_email_validator/java/Solution.java
+++ 04_email_validator/java/Solution.java
@@ -1,1 +1,14 @@
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter email: ");
+        String email = sc.nextLine();
+        String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,}$";
+        if(Pattern.matches(regex,email)) System.out.println("Valid Email");
+        else System.out.println("Invalid Email");
+    }
+}
 
