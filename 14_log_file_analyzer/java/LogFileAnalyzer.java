25b4a1ed3eb21ca9d43719e874d6b2ecabcb43bc
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-20 21:53:19 +0530

    Update LogFileAnalyzer.java

--- 14_log_file_analyzer/java/LogFileAnalyzer.java
+++ 14_log_file_analyzer/java/LogFileAnalyzer.java
@@ -2,13 +2,27 @@ import java.io.*;
 
 public class LogFileAnalyzer {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("app.log"));
-        int count = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter log file name: ");
+        String filename = br.readLine();
+
+        int error = 0, warning = 0, info = 0;
+
+        BufferedReader file = new BufferedReader(new FileReader(filename));
         String line;
 
-        while ((line = br.readLine()) != null) {
-            if (line.contains("ERROR")) count++;
+        while ((line = file.readLine()) != null) {
+            if (line.contains("ERROR")) error++;
+            else if (line.contains("WARNING")) warning++;
+            else if (line.contains("INFO")) info++;
         }
-        System.out.println("Error count: " + count);
+
+        file.close();
+
+        System.out.println("\nLog Summary:");
+        System.out.println("ERROR: " + error);
+        System.out.println("WARNING: " + warning);
+        System.out.println("INFO: " + info);
     }
 }
