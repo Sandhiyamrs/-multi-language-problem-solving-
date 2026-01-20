c51b5f931a8057219fe9b71cddc46dff1dd6a1e2
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-20 21:51:12 +0530

    Update student_grade_manager.py

--- 13_student_grade_manager/python/student_grade_manager.py
+++ 13_student_grade_manager/python/student_grade_manager.py
@@ -1,14 +1,34 @@
-marks = list(map(int, input("Enter marks: ").split()))
-avg = sum(marks) / len(marks)
+students = []
 
-if avg >= 90:
-    grade = "A"
-elif avg >= 75:
-    grade = "B"
-elif avg >= 60:
-    grade = "C"
-else:
-    grade = "Fail"
+while True:
+    print("\n1. Add Student")
+    print("2. View Students")
+    print("3. Show Average")
+    print("4. Exit")
 
-print("Average:", avg)
-print("Grade:", grade)
+    choice = input("Choose option: ")
+
+    if choice == "1":
+        name = input("Student name: ")
+        grade = float(input("Grade: "))
+        students.append((name, grade))
+        print("Student added.")
+
+    elif choice == "2":
+        if not students:
+            print("No students available.")
+        for name, grade in students:
+            print(f"{name}: {grade}")
+
+    elif choice == "3":
+        if students:
+            avg = sum(g for _, g in students) / len(students)
+            print("Class Average:", avg)
+        else:
+            print("No grades to calculate.")
+
+    elif choice == "4":
+        break
+
+    else:
+        print("Invalid choice.")
