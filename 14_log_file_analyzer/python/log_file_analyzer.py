a837b9c6a852202c86c8aa3cd9ed67ec543e3449
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-20 21:52:58 +0530

    Update log_file_analyzer.py

--- 14_log_file_analyzer/python/log_file_analyzer.py
+++ 14_log_file_analyzer/python/log_file_analyzer.py
@@ -1,6 +1,21 @@
-count = 0
-with open("app.log") as file:
-    for line in file:
-        if "ERROR" in line:
-            count += 1
-print("Error count:", count)
+filename = input("Enter log file name: ")
+
+error = warning = info = 0
+
+try:
+    with open(filename, "r") as file:
+        for line in file:
+            if "ERROR" in line:
+                error += 1
+            elif "WARNING" in line:
+                warning += 1
+            elif "INFO" in line:
+                info += 1
+
+    print("\nLog Summary:")
+    print("ERROR:", error)
+    print("WARNING:", warning)
+    print("INFO:", info)
+
+except FileNotFoundError:
+    print("Log file not found.")
